2d367b4fc43f8439ef5783cdf658c40a3c26e937
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2023-04-24 19:05:57 +0200

    Fixed bot telegram.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,10 @@
 import * as functions from "firebase-functions";
 import {initializeApp, cert} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
+import * as dotenv from "dotenv";
+
+dotenv.config({path: ".env"});
+
 import {Telegram} from "telegraf";
 const telegram: Telegram = new Telegram(process.env.BOT_TOKEN as string);
 
