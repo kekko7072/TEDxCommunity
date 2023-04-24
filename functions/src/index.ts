9085a3838f1b1db35da87d15db074158ca714d17
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2023-04-24 17:49:28 +0200

    Fixed some issue in license bot.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,22 @@
 import * as functions from "firebase-functions";
-import { Telegram } from "telegraf";
+import {Telegram} from "telegraf";
 const telegram: Telegram = new Telegram(process.env.BOT_TOKEN as string);
-const chatId = process.env.CHAT_ID as string; //TO GET CHATID: https://telegram.me/get_id_bot
+const chatId = process.env.CHAT_ID as string; // TO GET CHATID: https://telegram.me/get_id_bot
 
 export const SendTelegramMessageLicensecreated = functions
   .region("europe-west3")
   .firestore.document("licenses/{licenseId}")
   .onCreate(async (snap, context) => {
     try {
-      // Get the newly created user document's data
       await telegram.sendMessage(
         chatId,
-        `🚀 New user in TEDxCommunity App\nLicenseId: ${
-          context.params.licenseId
-        }\nName: ${snap.data().licenseName}\nAdmin: ${snap.data().adminUid}`
+        `🚀 New license activated in TEDxCommunity App
+        \n\n
+        LicenseId: ${context.params.licenseId}\n
+        Name: ${snap.data().licenseName}\n
+        Admin: ${snap.data().adminUid}\n
+        \n\n
+        `
       );
     } catch (error) {
       console.error(`Error sending Telegram message: ${error}`);
