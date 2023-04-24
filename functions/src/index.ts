e02207c4fa97f3f884f6e5b62ee44d3b9bca0a1f
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2023-04-24 17:40:20 +0200

    Added chat id.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,21 @@
 import * as functions from "firebase-functions";
-import {Telegram} from "telegraf";
+import { Telegram } from "telegraf";
 const telegram: Telegram = new Telegram(process.env.BOT_TOKEN as string);
-const chatId = "@kekko7072";
+const chatId = process.env.CHAT_ID as string;
 
 export const SendTelegramMessageLicensecreated = functions
   .region("europe-west3")
   .firestore.document("licenses/{licenseId}")
   .onCreate(async (snap, context) => {
-    await telegram.sendMessage(
-      chatId,
-      `🚀 New user in TEDxCommunity App\nLicenseId: ${
-        context.params.licenseId
-      }\nName: ${snap.data().licenseName}\nAdmin: ${snap.data().adminUid}`
-    );
+    try {
+      // Get the newly created user document's data
+      await telegram.sendMessage(
+        chatId,
+        `🚀 New user in TEDxCommunity App\nLicenseId: ${
+          context.params.licenseId
+        }\nName: ${snap.data().licenseName}\nAdmin: ${snap.data().adminUid}`
+      );
+    } catch (error) {
+      console.error(`Error sending Telegram message: ${error}`);
+    }
   });
