878f5e7451704a1299746c8c95e83fe58b1e981e
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2023-05-07 12:10:21 +0200

    Added video and credits.

--- lib/interface/widget/intro_alert_dialog.dart
+++ lib/interface/widget/intro_alert_dialog.dart
@@ -13,11 +13,21 @@ class _IntroAlertDialogState extends State<IntroAlertDialog> {
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.asset('assets/video.mp4')
+    _controller = VideoPlayerController.network(
+        'https://firebasestorage.googleapis.com/v0/b/tedxcommunity.appspot.com/o/video.mp4?alt=media&token=0b16e912-32aa-402c-bf23-0b17cc5f820f')
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
       });
+    _controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -36,98 +46,127 @@ class _IntroAlertDialogState extends State<IntroAlertDialog> {
                   middle: Text(
                       AppLocalizations.of(context)!.welcomeToTEDxCommunity),
                 ),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const SizedBox(height: 20),
-                      Stack(
-                        alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(height: 25),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 2.5,
+                      child: Stack(
                         children: [
                           GestureDetector(
                             onDoubleTap: () {
-                              setState(() {
-                                _controller.value.isPlaying
-                                    ? _controller.pause()
-                                    : _controller.play();
-                              });
+                              _controller.value.isPlaying
+                                  ? _controller.pause()
+                                  : _controller.play();
                             },
                             onTap: () {
-                              setState(() {
-                                _controller.value.isPlaying
-                                    ? _controller.pause()
-                                    : _controller.play();
-                              });
+                              _controller.value.isPlaying
+                                  ? _controller.pause()
+                                  : _controller.play();
                             },
-                            behavior: HitTestBehavior.opaque,
+                            behavior: HitTestBehavior.translucent,
                             child: _controller.value.isInitialized
-                                ? SizedBox(
-                                    height:
-                                        MediaQuery.of(context).size.height / 2,
-                                    child: VideoPlayer(_controller))
+                                ? VideoPlayer(_controller)
                                 : Container(),
                           ),
-                          Visibility(
-                            visible: !_controller.value.isPlaying,
-                            child: Center(
-                              child: CupertinoButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _controller.value.isPlaying
-                                        ? _controller.pause()
-                                        : _controller.play();
-                                  });
-                                },
-                                child: Icon(
-                                  CupertinoIcons.play_circle_fill,
-                                  color: Style.primaryColor,
-                                  size: 100,
+                          Align(
+                            alignment: Alignment.center,
+                            child: Visibility(
+                              visible: !_controller.value.isPlaying,
+                              child: Center(
+                                child: CupertinoButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _controller.value.isPlaying
+                                          ? _controller.pause()
+                                          : _controller.play();
+                                    });
+                                  },
+                                  child: Icon(
+                                    CupertinoIcons.play_circle_fill,
+                                    color: Style.primaryColor,
+                                    size: 100,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: StreamBuilder<Duration?>(
+                                stream: _controller.position.asStream(),
+                                builder: (context, snapshot) {
+                                  return Visibility(
+                                    visible: !_controller.value.isPlaying ||
+                                        _controller.value.isPlaying &&
+                                            (snapshot.data?.inSeconds ?? 0) < 5,
+                                    child: CupertinoButton(
+                                      padding: EdgeInsets.zero,
+                                      onPressed: () async {
+                                        await launchUrlString(
+                                            'https://dalcantonandrea.myportfolio.com/');
+                                      },
+                                      child: Card(
+                                          color: Style.primaryColor
+                                              .withOpacity(0.8),
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(4.0),
+                                            child: Text(
+                                              'video by\nAndrea Dal Canton',
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.w500,
+                                                  color: Colors.grey[200]),
+                                            ),
+                                          )),
+                                    ),
+                                  );
+                                }),
+                          ),
                         ],
                       ),
-                      Visibility(
-                        visible: _controller.value.isPlaying,
-                        child: IconButton(
-                            onPressed: () =>
-                                setState(() => _controller.pause()),
-                            icon: Icon(
-                              CupertinoIcons.stop_circle_fill,
-                              color: Style.primaryColor,
-                            )),
-                      ),
-                      Text(
-                        AppLocalizations.of(context)!.whatIsTEDxCommunity,
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium!
-                            .copyWith(color: Style.textColor(context)),
-                      ),
-                      /* Text(AppLocalizations.of(context)!.learnMoreAboutTEDxCommunity),
+                    ),
+                    Visibility(
+                      visible: _controller.value.isPlaying,
+                      child: IconButton(
+                          onPressed: () => _controller.pause(),
+                          icon: Icon(
+                            CupertinoIcons.stop_circle_fill,
+                            color: Style.primaryColor,
+                          )),
+                    ),
+                    Text(
+                      AppLocalizations.of(context)!.whatIsTEDxCommunity,
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium!
+                          .copyWith(color: Style.textColor(context)),
+                      textAlign: TextAlign.center,
+                    ),
+                    /* Text(AppLocalizations.of(context)!.learnMoreAboutTEDxCommunity),
             Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: SelectableText(
-                        kGitHubReadmeLink,
-                        style: const TextStyle(color: CupertinoColors.link),
-                        onTap: () => launchUrlString(kGitHubReadmeLink),
-                      ),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: SelectableText(
+                      kGitHubReadmeLink,
+                      style: const TextStyle(color: CupertinoColors.link),
+                      onTap: () => launchUrlString(kGitHubReadmeLink),
+                    ),
             ),*/
-                      const SizedBox(height: 16.0),
-                      Text(
-                        '${AppLocalizations.of(context)!.developedWithLoveBy} ${AppLocalizations.of(context)!.developerName}',
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium!
-                            .copyWith(color: Style.textColor(context)),
-                      ),
-                      CupertinoDialogAction(
-                        child: Text(AppLocalizations.of(context)!.close),
-                        onPressed: () => Navigator.of(context).pop(),
-                      ),
-                    ],
-                  ),
+                    const SizedBox(height: 16.0),
+                    Text(
+                      '${AppLocalizations.of(context)!.developedWithLoveBy} ${AppLocalizations.of(context)!.developerName}',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium!
+                          .copyWith(color: Style.textColor(context)),
+                      textAlign: TextAlign.center,
+                    ),
+                    CupertinoDialogAction(
+                      child: Text(AppLocalizations.of(context)!.close),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                  ],
                 ),
               ),
             )));
